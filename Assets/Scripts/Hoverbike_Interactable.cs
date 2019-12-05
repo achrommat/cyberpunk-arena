9b5a85cd0304e816d5de3effb476bd0319f6e1da
Roman Paramonov <r.paramonov@kefirgames.com>
2019-12-05 19:01:06 +0400

    - add: hoverbike;

---
+++ Assets/Scripts/Hoverbike_Interactable.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hoverbike_Interactable : MonoBehaviour
+{
+    public GameObject explosive;
+    public GameObject trail;
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    public void RunTrail()
+    {
+        trail.GetComponent<ParticleSystem>().Play();
+    }
+
+    public void RunExplosion()
+    {
+        Instantiate(explosive, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
