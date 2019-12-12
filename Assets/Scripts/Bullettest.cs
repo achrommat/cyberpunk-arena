1f19ade1a27f3ccad9eaa287fa758b6f1a74ebe8
Roman Paramonov <r.paramonov@kefirgames.com>
2019-12-12 18:22:35 +0400

    - cgh: hoverbike animation;

--- Assets/Scripts/Bullettest.cs
+++ Assets/Scripts/Bullettest.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Bullettest : MonoBehaviour
 {
+    public int damage;
     float speed = 60;
     public GameObject HitVFX;
     public GameObject HitDecal;
@@ -14,6 +15,13 @@ public class Bullettest : MonoBehaviour
     }
     public void OnTriggerEnter(Collider other)
     {
+        if (other.CompareTag("Interactable"))
+        {
+            GameObject hit = Instantiate(HitVFX, transform.position, Quaternion.LookRotation(transform.position, other.transform.position));
+            Destroy(gameObject);
+            other.GetComponent<Hoverbike_Interactable>().health -= damage;
+        }
+
         if (other.CompareTag("Walls"))
         {
             //тут надо добить поворот попадения - Quaternion или забить хуй (пока второй вариант)
--- Assets/Scripts/Hoverbike_Interactable.cs
+++ Assets/Scripts/Hoverbike_Interactable.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Hoverbike_Interactable : MonoBehaviour
 {
+    public int health;
     public GameObject explosive;
     public GameObject trail;
     private Animator anim;
@@ -22,5 +23,18 @@ public class Hoverbike_Interactable : MonoBehaviour
     {
         Instantiate(explosive, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
         Destroy(gameObject);
+    }
+
+    private void Update()
+    {
+        if (health < 1 && !anim.GetBool("Run_Animation"))
+        {
+            Activate();
+        }
+    }
+
+    private void Activate()
+    {
+        anim.SetBool("Run_Animation", true);
     }
 }
