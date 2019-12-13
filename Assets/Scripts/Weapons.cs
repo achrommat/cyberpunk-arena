06ba0f0e2b5105a0a7a28f5d2bd2a9ba646aaedd
Anton Harybin <a.harybin@kefirgames.com>
2019-12-13 16:06:06 +0400

    -add: weapon_audio_and_stats;

--- Assets/Scripts/Weapons.cs
+++ Assets/Scripts/Weapons.cs
@@ -1,19 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class Weapons : MonoBehaviour
 {
     public GameObject bullet;
     public GameObject ShootPos;
     public GameObject ShootFX;
-    public GameObject AudioSource;
+    public AudioSource Source;
+    public AudioClip Clip;
     public float speed;
+    public float scatter =0;
     public float BulletLifeTime =0.5f;
     public bool shooting;
 
     public float ShootCD= 1;
-    public float ShootTimer = 0;
+    protected float ShootTimer = 0;
 
     public void Update()
     {
@@ -28,10 +31,13 @@ public class Weapons : MonoBehaviour
     }
     public void Shoot()
     {
+        Source.PlayOneShot(Clip);
+        ShootPos.transform.localRotation = Quaternion.Euler(Random.Range(-scatter, scatter), Random.Range(-scatter, scatter), 0);
         GameObject newbullet = Instantiate(bullet, ShootPos.transform.position, ShootPos.transform.rotation);
-        GameObject newAudioSource = Instantiate(AudioSource, transform.position, transform.rotation);
+        //GameObject newAudioSource = Instantiate(AudioSource, transform.position, transform.rotation);
+
         Destroy(newbullet, BulletLifeTime);
-        Destroy(newAudioSource, 2);
+        //Destroy(newAudioSource, 2);
         ShootTimer = ShootCD;
     }
 }
