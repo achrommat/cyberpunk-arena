24b8f8e6655c45ae39bb48ea8add21b15d34fbc1
Anton Harybin <a.harybin@kefirgames.com>
2019-12-15 17:52:52 +0400

    -add: ammo_gui;

---
+++ Assets/StatsGui.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StatsGui : MonoBehaviour
+{
+    public GameObject Weapons;
+    void FixedUpdate()
+    {
+        for(int i = 0; i < Weapons.transform.childCount; i++)
+        {
+            if(Weapons.transform.GetChild(i).gameObject.active)
+            {
+              transform.GetChild(0).GetComponent<Text>().text = ""+ (Weapons.transform.GetChild(i).GetComponent<Weapons>().Ammo);
+            }
+        }
+    }
+}
