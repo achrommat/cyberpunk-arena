a818031e47a168ca389c663b05723befe5b6857b
Anton Harybin <a.harybin@kefirgames.com>
2019-12-16 12:42:45 +0400

    -add: ripper_gun_prototipe;

--- Assets/Scripts/Bullettest.cs
+++ Assets/Scripts/Bullettest.cs
@@ -2,16 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Bullettest : MonoBehaviour
 {
+    public LayerMask collisionMask;
     public int damage;
-    float speed = 60;
+    public float speed = 60;
     public GameObject HitVFX;
     public GameObject HitDecal;
+    public int ricochet = 0;
 
      void FixedUpdate()
     {
        GetComponent<Rigidbody>().MovePosition(transform.position + transform.forward * speed * Time.fixedDeltaTime);
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+        if(Physics.Raycast(ray,out hit,Time.deltaTime * speed + .1f))
+        {
+            Vector3 reflectDir = Vector3.Reflect(ray.direction, hit.normal);
+            float rot = 90 - Mathf.Atan2(reflectDir.z, reflectDir.x) * Mathf.Rad2Deg;
+            transform.eulerAngles = new Vector3(0, rot, 0);
+        }
     }
     public void OnTriggerEnter(Collider other)
     {
@@ -24,10 +35,25 @@ public class Bullettest : MonoBehaviour
 
         if (other.CompareTag("Walls"))
         {
-            //тут надо добить поворот попадения - Quaternion или забить хуй (пока второй вариант)
-            //возможно надо делать через рейкаст (пример в PrBullet)
-            GameObject hit = Instantiate(HitVFX, transform.position, Quaternion.LookRotation(transform.position, other.transform.position));
-            Destroy(gameObject);
+           
+
+          
+            if (ricochet > 0)
+            {
+                Vector3 reflectDir = Vector3.Reflect(transform.position.normalized, other.transform.position.normalized);
+                float rot = 90 - Mathf.Atan2(reflectDir.z, reflectDir.x) * Mathf.Rad2Deg;
+                transform.eulerAngles = new Vector3(0, rot, 0);
+
+            }
+            else
+            {
+                //тут надо добить поворот попадения - Quaternion или забить хуй (пока второй вариант)
+                //возможно надо делать через рейкаст (пример в PrBullet)
+                GameObject hit = Instantiate(HitVFX, transform.position, Quaternion.LookRotation(transform.position, other.transform.position));
+                Destroy(gameObject);
+            }
+
+
         }
     }
 }
