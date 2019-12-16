121a601fe47425f8e3ae6235d819ac3d42206076
Roman Paramonov <r.paramonov@kefirgames.com>
2019-12-16 18:22:03 +0400

    - add: new_packs; nav_mesh_draft;

---
+++ Assets/Scripts/ExplosiveBotController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ExplosiveBotController : MonoBehaviour
+{
+
+    private Transform target;
+    private NavMeshAgent agent;
+    
+    void Start()
+    {
+        target = PlayerManager.instance.player.transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        agent.SetDestination(target.position);
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class GameManager : MonoBehaviour
+{
+
+    private NavMeshSurface surface;
+
+    void Start()
+    {
+        surface = FindObjectOfType<NavMeshSurface>();
+        //surface.BuildNavMesh();
+    }
+}
