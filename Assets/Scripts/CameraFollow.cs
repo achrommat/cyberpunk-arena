d953f089c2b76b28ce0f966d9bc9a0ee91e57c6d
Anton Harybin <a.harybin@kefirgames.com>
2019-12-20 12:30:06 +0400

    -add: camera;

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,13 +4,10 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    GameObject player;
+    public GameObject player;
     public float distance;
     public float distance2;
-    private void Start()
-    {
-        player = GameObject.FindGameObjectWithTag("Player");
-    }
+    
     private void Update()
     {
         transform.position = new Vector3(player.transform.position.x, player.transform.position.y +distance, player.transform.position.z + distance2);
