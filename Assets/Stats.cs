adf68a1f5a7b31175d333eb5ec0f9c8532e24691
Anton Harybin <a.harybin@kefirgames.com>
2020-01-26 18:36:29 +0400

    -add: stats_and_player_controller;

---
+++ Assets/Stats.cs
@@ -0,0 +1,41 @@
+﻿using BehaviorDesigner.Runtime.Tactical;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stats : MonoBehaviour, IDamageable
+{
+    [Header("Main Stats")]
+    public float Health;
+    float healthCount;
+    public float runSpeed;
+    public float startRunSpeed;
+    public float speedWithAim;
+    public float runSpeedMody;
+    //public float speedaim;
+    public float speedsetting = 1;
+
+    void IDamageable.Damage(float amount)
+    {
+        Health--;
+        //throw new System.NotImplementedException();
+    }
+
+    bool IDamageable.IsAlive()
+    {
+        return Health > 0;
+        //throw new System.NotImplementedException();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        runSpeed = (startRunSpeed + speedWithAim + runSpeedMody);
+    }
+}
