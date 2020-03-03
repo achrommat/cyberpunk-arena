e3abfd03d5ee792c3ee24b58fcd8342361ccdef3
Roman Paramonov <r.paramonov@kefirgames.com>
2020-03-03 14:18:35 +0400

    - character generator draft ;

---
+++ Assets/_Scripts/Player/Characters/Character.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "New Character", menuName = "Characters/New Character", order = 1)]
+public class Character : ScriptableObject
+{
+    public string Name = "New Character";
+    public string Description = "Character's description";
+    public Image Icon;
+    public float DropProbability;
+    public MeshRenderer Skin;
+
+    public Perk[] Perks;
+    public int PerkCount = 3;
+
+    [SerializeField] private Perk[] _allPerks;
+
+    private void OnEnable()
+    {
+        GetAllPerks();
+        GetRandomPerks();
+    }
+
+    public void GetAllPerks()
+    {
+        _allPerks = Resources.FindObjectsOfTypeAll<Perk>();
+    }
+
+    private void GetRandomPerks()
+    {
+        Perks = new Perk[PerkCount];
+        for (int i = 0; i < PerkCount; i++)
+        {
+            // Take only from the latter part of the list - ignore the first i items.
+            int take = Random.Range(i, _allPerks.Length);
+            Perks[i] = _allPerks[take];
+
+            // Swap our random choice to the beginning of the array,
+            // so we don't choose it again on subsequent iterations.
+            _allPerks[take] = _allPerks[i];
+            _allPerks[i] = Perks[i];
+        }
+    }
+}
