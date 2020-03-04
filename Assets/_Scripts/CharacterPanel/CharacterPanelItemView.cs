e1f5481b0acea2027a9a6203e340c19bd01e4701
Roman Paramonov <r.paramonov@kefirgames.com>
2020-03-04 14:44:44 +0400

    - character panel draft ;

---
+++ Assets/_Scripts/CharacterPanel/CharacterPanelItemView.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterPanelItemView : MonoBehaviour
+{
+    public Button Button;
+    public Image ItemIcon;
+    public Text Description;
+
+    private Character _characterData;
+
+    public void InitItem(Character character)
+    {
+        _characterData = character;
+        Description.text = character.Description;
+
+        Button.onClick.AddListener(ButtonClicked);
+    }
+
+    private void ButtonClicked()
+    {
+        Debug.Log(_characterData.name);
+    }
+}
---
+++ Assets/_Scripts/CharacterPanel/CharacterPanelViewController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterPanelViewController : MonoBehaviour
+{
+
+    [SerializeField] private Transform _characterPanel;
+    [SerializeField] private CharacterPanelItemView[] _characterItems;
+    [SerializeField] private int _characterCount;
+    [SerializeField] private List<Character> _allCharacters;
+    public Character[] Characters;
+
+    void OnEnable()
+    {
+        GetAllCharacters();
+        GetRandomCharacters();
+    }
+
+    public void GetAllCharacters()
+    {
+        string[] assetNames = AssetDatabase.FindAssets("t:Character", new[] { "Assets/Resources/_ScriptableObjects/Characters" });
+        _allCharacters.Clear();
+        foreach (string SOName in assetNames)
+        {
+            var SOpath = AssetDatabase.GUIDToAssetPath(SOName);
+            var character = AssetDatabase.LoadAssetAtPath<Character>(SOpath);
+            _allCharacters.Add(character);
+        }
+        //_allCharacters = Resources.FindObjectsOfTypeAll<Character>();
+    }
+
+    private void GetRandomCharacters()
+    {
+        Characters = new Character[_characterCount];
+        for (int i = 0; i < _characterCount; i++)
+        {
+            // Take only from the latter part of the list - ignore the first i items.
+            int take = Random.Range(i, _allCharacters.Count);
+            Characters[i] = _allCharacters[take];
+
+            // Swap our random choice to the beginning of the array,
+            // so we don't choose it again on subsequent iterations.
+            _allCharacters[take] = _allCharacters[i];
+            _allCharacters[i] = Characters[i];
+        }
+
+        InitializeCharacterPanelItem();
+
+    }
+
+    private void InitializeCharacterPanelItem()
+    {
+        _characterItems = _characterPanel.GetComponentsInChildren<CharacterPanelItemView>();
+        for (int i = 0; i < _characterItems.Length; i++)
+        {
+            _characterItems[i].InitItem(Characters[i]);
+        }
+    }
+
+
+}
